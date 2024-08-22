a7493cae0f9406bd8af39e1ff5cea1c3feb1598b
Jordan Freundlich <jordan.freundlich@gmail.com>
2024-08-22 11:12:30 -0400

    configured auth with cognito

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,26 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { inter } from "@/ui/fonts";
+import type { Metadata } from 'next';
+import './globals.css';
+import { inter } from '@/ui/fonts';
+import ConfigureAmplifyClientSide from './amplify-cognito-config';
 
 export const metadata: Metadata = {
-  title: "Next.js Cognito Authentication",
-  description: "Cognito authenticated Next.js app.",
+	title: 'Next.js Cognito Authentication',
+	description: 'Cognito authenticated Next.js app.',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className={`${inter.className} antialiased`}>
+				<>
+					<ConfigureAmplifyClientSide />
+					{children}
+				</>
+			</body>
+		</html>
+	);
 }
